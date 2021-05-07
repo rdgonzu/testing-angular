e6112606b7315173f2885274283437eca633ceb3
René González <rdgonzu@gmail.com>
2021-05-06 23:37:20 -0400

    Adding routing.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {routing, appRoutingProviders} from './app.routing';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SliderComponent } from './components/slider/slider.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { FormComponent } from './components/form/form.component';
+import { PageComponent } from './components/page/page.component';
 
 @NgModule({
   declarations: [
@@ -13,12 +18,17 @@ import { FooterComponent } from './components/footer/footer.component';
     HeaderComponent,
     SliderComponent,
     SidebarComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    BlogComponent,
+    FormComponent,
+    PageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
-  providers: [],
+  providers: [appRoutingProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.routing.ts
@@ -0,0 +1,22 @@
+//Imports modules from angular's router.
+import {ModuleWithProviders} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+
+//Imports components that will be pages.
+import {HomeComponent} from './components/home/home.component';
+import {BlogComponent} from './components/blog/blog.component';
+import {FormComponent} from './components/form/form.component';
+import {PageComponent} from './components/page/page.component';
+
+//Defines routes array.
+const appRoutes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'home', component: HomeComponent},
+    {path: 'blog', component: BlogComponent},
+    {path: 'form', component: FormComponent},
+    {path: 'page', component: PageComponent}
+];
+
+//Exports module.
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders<RouterModule> = RouterModule.forRoot(appRoutes);
